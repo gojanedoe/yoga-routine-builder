9037ce03d30a2baa6ad943974b43a9c9690b69de
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-12-10 18:33:21 -0600

    Add prettier config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@material-ui/core';
-import Grid  from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 import Item from '@material-ui/core/Grid';
 import { borders } from '@material-ui/system';
 import Paper from '@material-ui/core/Paper';
@@ -11,37 +11,19 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <div >
-      <Grid 
-        container 
-        spacing={2}
-        
-        >
-        <Grid 
-          className="poseList"
-          container 
-          item xs={4}
-
-        >
-          <Paper>xs=8</Paper>
-          
+      <div>
+        <Grid container spacing={2}>
+          <Grid className="poseList" container item xs={4}>
+            <Paper>xs=8</Paper>
+          </Grid>
+          <Grid container item xs={4}>
+            <Box border={5}>xs=8</Box>
+          </Grid>
+          <Grid container item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>
         </Grid>
-        
-        <Grid container item xs={4}>
-          <Box
-            border={5}>
-              xs=8
-          </Box>
-        </Grid>
-        
-        <Grid container item xs={4}>
-          <Item>xs=8</Item>
-        </Grid>
-
-      </Grid>
-        
       </div>
-      
     </React.Fragment>
   );
 }
