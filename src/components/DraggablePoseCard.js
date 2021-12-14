18fc82bc4a8476678d693219ef1918533897c9b6
Holly Henske <hcover333@gmail.com>
2021-12-14 07:50:05 -0600

    used a table to get image in desired location

--- src/components/DraggablePoseCard.js
+++ src/components/DraggablePoseCard.js
@@ -13,21 +13,29 @@ const DraggablePoseCard = () => {
 
     return (
         <Card sx={{ maxWidth: 300 }} elevation={3}>
-        <CardContent>
-        
-          <Typography variant="h5" component="div">
-            Pose Name
-            {/* {actual pose name will be in curly braces here instead of "Pose Name"} */}
-          </Typography>
-          
-        {/* for delete icon and button, this video takes through coding: https://www.youtube.com/watch?v=M75MUZ1zVYM */}
-          
-        </CardContent> 
+          <CardContent>
+            <table>
+              <tr>
+                <td>
+                  <Typography variant="h5" component="div">
+                  Pose Name
+                  {/* {actual pose name will be in curly braces here instead of "Pose Name"} */}
+                  </Typography>
+                </td>
+                <td>     </td>
+                <td>
+                  <img src="images/treeposegeneric.png" alt="pose name" />
+                  {/* for delete icon and button, this video takes through coding: https://www.youtube.com/watch?v=M75MUZ1zVYM */}
+                </td>
+            </tr>
+          </table>
+          </CardContent> 
       
-        <CardActions>
-          <Button size="small">Learn More</Button>
-          {/* This will open pose information */}
-        </CardActions>
+          <CardActions>
+            
+            <Button size="small">Learn More</Button>
+            {/* This will open pose information */}
+          </CardActions>
       </Card>
     )
 }
