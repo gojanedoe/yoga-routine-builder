c912497f5cbc11c74c604c17bfabda9b4026cfd4
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-12-15 17:11:18 -0600

    Make both columns work with drag and drop

--- src/App.js
+++ src/App.js
@@ -35,41 +35,51 @@ function App() {
     // If pose is not dragged to a valid destination, keep list the same
     if (!result.destination) return;
 
-    console.log('Result.source: ', result.source.droppableId);
-    console.log('Result.destination: ', result.destination.droppableId);
+    const source = result.source.droppableId;
+    const sourceIndex = result.source.index;
+    const destination = result.destination.droppableId;
+    const destinationIndex = result.destination.index;
 
-    if (
-      result.source.droppableId === 'column-1' &&
-      result.destination.droppableId === 'column-1'
-    ) {
+    if (source === destination) {
       // Grab poses from original list
-      const items = Array.from(poses);
+      const items = Array.from(source === 'column-1' ? poses : routine);
 
       // Grab moving pose & remove from list
-      const [reorderedItem] = items.splice(result.source.index, 1);
+      const [reorderedItem] = items.splice(sourceIndex, 1);
 
       // Add moving pose to new spot (index) in list
-      items.splice(result.destination.index, 0, reorderedItem);
+      items.splice(destinationIndex, 0, reorderedItem);
 
       //Update state
-      updatePoses(items);
-    } else if (
-      result.source.droppableId === 'column-1' &&
-      result.destination.droppableId === 'column-2'
-    ) {
+      source === 'column-1' ? updatePoses(items) : updateRoutine(items);
+    } else if (source === 'column-1' && destination === 'column-2') {
       // Grab poses from original lists
       const fromItems = Array.from(poses);
       const toItems = Array.from(routine);
 
       // Grab moving pose & remove from list
-      const [reorderedItem] = fromItems.splice(result.source.index, 1);
+      const [reorderedItem] = fromItems.splice(sourceIndex, 1);
 
       // Add moving pose to new spot (index) in list
-      toItems.splice(result.destination.index, 0, reorderedItem);
+      toItems.splice(destinationIndex, 0, reorderedItem);
 
       //Update state
       updatePoses(fromItems);
       updateRoutine(toItems);
+    } else if (source === 'column-2' && destination === 'column-1') {
+      // Grab poses from original lists
+      const fromItems = Array.from(routine);
+      const toItems = Array.from(poses);
+
+      // Grab moving pose & remove from list
+      const [reorderedItem] = fromItems.splice(sourceIndex, 1);
+
+      // Add moving pose to new spot (index) in list
+      toItems.splice(destinationIndex, 0, reorderedItem);
+
+      //Update state
+      updatePoses(toItems);
+      updateRoutine(fromItems);
     }
   };
 
