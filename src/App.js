ac4801809e4c37c285a754cbb34034a216cd90f6
BeltedKingfisher <amyjseibert@gmail.com>
2021-12-15 20:27:47 -0600

    Initial Ugly Modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
@@ -5,11 +6,22 @@ import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
+=======
+import React from "react";
+import "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Item from "@material-ui/core/Grid";
+import { borders } from "@material-ui/system";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import InfoModal from "./InfoModal.js";
+>>>>>>> Initial Ugly Modal
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
+<<<<<<< HEAD
     <Container className="landingContainer">
       <Grid
         container
@@ -44,6 +56,26 @@ function App() {
         <Button variant="outlined">Save Routine</Button>
       </Grid>
     </Container>
+=======
+    <React.Fragment>
+      <div>
+        <InfoModal />
+        <Grid container spacing={2}>
+          <Grid className="poseList" container item xs={4}>
+            <Paper>xs=8</Paper>
+          </Grid>
+
+          <Grid container item xs={4}>
+            <Box border={5}>xs=8</Box>
+          </Grid>
+
+          <Grid container item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+>>>>>>> Initial Ugly Modal
   );
 }
 
