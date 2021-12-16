65e98366e34a70cbec78fb2ab2854370237a85d5
BeltedKingfisher <amyjseibert@gmail.com>
2021-12-15 20:27:47 -0600

    Initial Ugly Modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from 'react';
 import DraggablePoseCard from './components/DraggablePoseCard';
 import PoseBank from './components/PoseBank';
@@ -11,8 +12,18 @@ import Button from '@material-ui/core/Button';
 import yogaPoses from './data/yogaPoses.json';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+=======
+import React from "react";
+import "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Item from "@material-ui/core/Grid";
+import { borders } from "@material-ui/system";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import InfoModal from "./InfoModal.js";
+>>>>>>> Initial Ugly Modal
 
-import './App.css';
+import "./App.css";
 
 // const dummyPoses = [
 //   { id: 'pose-1', name: 'Pose 1' },
@@ -86,6 +97,7 @@ function App() {
   };
 
   return (
+<<<<<<< HEAD
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Container className="landingContainer">
         <Grid
@@ -113,6 +125,26 @@ function App() {
         </Grid>
       </Container>
     </DragDropContext>
+=======
+    <React.Fragment>
+      <div>
+        <InfoModal />
+        <Grid container spacing={2}>
+          <Grid className="poseList" container item xs={4}>
+            <Paper>xs=8</Paper>
+          </Grid>
+
+          <Grid container item xs={4}>
+            <Box border={5}>xs=8</Box>
+          </Grid>
+
+          <Grid container item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+>>>>>>> Initial Ugly Modal
   );
 }
 
