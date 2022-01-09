588d37bb2880589871a773b7e2e9148196d817d3
BeltedKingfisher <amyjseibert@gmail.com>
2022-01-09 16:10:45 -0600

    starting Timer build use this one

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { React, useState } from 'react';
 
 const Timer = props => {
@@ -13,6 +14,39 @@ const Timer = props => {
       })}
     </div>
   );
+=======
+import { useEffect } from 'react';
+
+const Timer = props => {
+  const {
+    timerIsRunning,
+    timerSeconds,
+    setTimerSeconds,
+    handleNextPhoto,
+    resetTimer
+  } = props;
+
+  // Referenced from https://upmostly.com/tutorials/build-a-react-timer-component-using-hooks
+  useEffect(() => {
+    let interval = null;
+
+    if (timerIsRunning && timerSeconds !== 0) {
+      interval = setInterval(() => {
+        setTimerSeconds(prevSeconds => prevSeconds - 1);
+      }, 1000);
+    } else if (timerIsRunning && timerSeconds <= 0) {
+      handleNextPhoto('Next');
+      resetTimer();
+    } else if (!timerIsRunning && timerSeconds !== 0) {
+      clearInterval(interval);
+    }
+
+    // Clear interval if the component is unmounted
+    return () => clearInterval(interval);
+  }, [timerIsRunning, timerSeconds]);
+
+  return <div className="Timer">:{timerSeconds}</div>;
+>>>>>>> starting Timer build use this one
 };
 
 export default Timer;
