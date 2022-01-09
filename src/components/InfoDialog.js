a25809b773c59469171e6f1528a04c358a1a98b4
BeltedKingfisher <amyjseibert@gmail.com>
2022-01-09 16:09:23 -0600

    starting Timer build

--- src/components/InfoDialog.js
+++ src/components/InfoDialog.js
@@ -1,7 +1,6 @@
 import { React, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -16,99 +15,25 @@ function InfoDialog(props) {
     selectedPose,
     updateRoutine,
     poseCounter,
-    setPoseCounter,
-    addedTime,
-    setAddedTime,
-    routine,
-    totalTime,
-    setTotalTime
+    setPoseCounter
   } = props;
 
-  // const [currentValue, setCurrentValue] = useState(1);
-
   const handleClose = () => {
-    setModalOpen(!modalOpen);
-  };
-
-  const addTimeHandler = event => {
-    setAddedTime(Number(event.target.value) - selectedPose.defaultTime);
-    //setTotalTime(selectedPose.defaultTime + addedTime);
+    setModalOpen(false);
   };
 
   const handleAddToRoutine = e => {
     updateRoutine(prevState => {
       // Create new pose id so there are no repeating keys in poses
       let newPoseId = `pose-${poseCounter + 1}`;
-      let newPose = {
-        ...selectedPose,
-        id: newPoseId,
-        addedTime: Number(addedTime)
-        //totalTime: Number(totalTime)
-      };
+      let newPose = { ...selectedPose, id: newPoseId };
 
       prevState.push(newPose);
       return prevState;
     });
 
     setPoseCounter(poseCounter + 1);
-<<<<<<< HEAD
     setModalOpen(false);
-    // setAddedTime(0);
-  };
-
-  const addTimeHandler = event => {
-    // Check right column poses (routine state) to find the current pose's placement
-
-    setCurrentIndex(routine.findIndex(pose => pose.id === selectedPose.id));
-    //let currentIndex = routine.findIndex(pose => pose.id === selectedPose.id);
-
-    setAddedTime2(event.target.value);
-
-    console.log('current index', currentIndex);
-
-<<<<<<< HEAD
-    if (event.target.value > 0) {
-      setAddedTime(event.target.value);
-    } else {
-      setAddedTime(0);
-    }
-<<<<<<< HEAD
-=======
-=======
-    // Update routine with newly timed pose
-    // setAddedTime(event.target.value);
-    let updatedPose = {
-      ...selectedPose,
-      addedTime: Number(event.target.value)
-    };
-
-    updateRoutine(prevRoutine => {
-      prevRoutine.splice(currentIndex, 1, updatedPose);
-      return prevRoutine;
-    });
-
->>>>>>> Add and save time to individual poses
-    console.log(event.target.value);
-    console.log('selected pose:', {
-      ...selectedPose,
-      addedTime: event.target.value
-    });
-<<<<<<< HEAD
->>>>>>> some pair programming
-=======
-
-    console.log(routine);
-    console.log(
-      'routine added time at curr index: ',
-      routine[currentIndex].addedTime
-    );
->>>>>>> Add and save time to individual poses
-=======
-    setModalOpen(!modalOpen);
-    setAddedTime(0);
-    //setTotalTime(0);
-    console.log(routine);
->>>>>>> applied new timer logic
   };
 
   return (
@@ -132,25 +57,15 @@ function InfoDialog(props) {
           </DialogContentText>
           <TextField
             autoFocus
-            className="textField"
             variant="outlined"
             margin="dense"
-            id={selectedPose.id}
-            label="Seconds in pose"
-            type="number"
-            placeholder={`${selectedPose.defaultTime} seconds`}
-            helperText={`Adjust time here.`}
-            // defaultValue={selectedPose.defaultTime}
-            //idea: helper text to give default time, then user chooses added seconds
-            //value={addedTime} //routine[currentIndex].addedTime
-            inputProps={{
-              min: selectedPose.defaultTime,
-              max: 500,
-              step: 1
-            }}
-            onChange={addTimeHandler}
+            id="seconds"
+            label="Length of Pose"
+            type="time"
+            min=":00"
+            max="1:00"
+            step="1"
           />
-          {/* needs capturing and sending over to Timer component */}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
