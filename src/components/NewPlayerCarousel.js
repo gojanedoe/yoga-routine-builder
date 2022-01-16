0d15c60c45e7345e4b82db1519c56f3f078c62dd
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2022-01-16 17:04:02 -0500

    Map routine's pose data to current slide and grab index

--- src/components/NewPlayerCarousel.js
+++ src/components/NewPlayerCarousel.js
@@ -1,35 +1,29 @@
-import { React, useState } from "react";
+import { React, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import yogaPoses from '../data/yogaPoses.json';
-import "./NewPlayerCarousel.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './NewPlayerCarousel.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 // used https://www.positronx.io/react-responsive-carousel-tutorial/ to help code, BUT I'm using css from a library
 // and there are elements I want/need to change
 //help with customizing: http://react-responsive-carousel.js.org/storybook/?path=/story/02-advanced--with-external-controls
 
+export default function NewPlayerCarousel({routine}) {
+  const [currImg, setCurrImg] = useState(0);
 
-export default function NewPlayerCarousel() {
+  const updateSlide = index => {
+    console.log(index);
+    console.log('pose properties: ', routine[index]);
+  };
 
-    const [currImg, setCurrImg] = useState(0)
-
-    return (
-        
-        <div className="carousel-wrapper">
-            <Carousel>
-                {/* here we would write the logic for displaying the poses in order from the json / user input */}
-                <img src={yogaPoses[0].image} />
-                <img src={yogaPoses[1].image} />
-                <img src={yogaPoses[2].image} />
-                <img src={yogaPoses[3].image} />
-                <img src={yogaPoses[4].image} />
-                <img src={yogaPoses[5].image} />
-                <img src={yogaPoses[6].image} />
-                <img src={yogaPoses[7].image} />
-                <img src={yogaPoses[8].image} />
-                <img src={yogaPoses[9].image} />
-            </Carousel>
-        </div>
-    );
+  return (
+    <div className="carousel-wrapper">
+      <Carousel onChange={updateSlide}>
+        {routine.map(pose => {
+          return <img src={pose.image} />;
+        })}
+      </Carousel>
+    </div>
+  );
 }
--- src/components/SimpleDialog.js
+++ src/components/SimpleDialog.js
@@ -7,27 +7,30 @@ import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
 import PoseTimer from './PoseTimer';
 import Grid from '@material-ui/core/Grid';
-import "./PoseTimer.css";
- 
- 
+import './PoseTimer.css';
+
 function SimpleDialog(props) {
   const { onClose, routine, open } = props;
   const handleClose = () => {
     onClose(routine);
   };
- 
+
   const time = new Date();
-    time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
- 
-       
-     
- 
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+
   return (
-    <Dialog fullScreen onClose={handleClose} open={open} style={{textAlign: 'center'}}>
-      <Grid Title><h1>Play your routine</h1></Grid>
+    <Dialog
+      fullScreen
+      onClose={handleClose}
+      open={open}
+      style={{ textAlign: 'center' }}
+    >
+      <Grid Title>
+        <h1>Play your routine</h1>
+      </Grid>
       <Grid container spacing={2}>
         <Grid item xs={1}></Grid>
-        <Grid item xs={3}>      
+        <Grid item xs={3}>
           <item>
             <AudioPlayer
               elevation={1}
@@ -35,15 +38,17 @@ function SimpleDialog(props) {
               variation="primary"
               spacing={3}
               src="Assets/tree_(vrkasana).mp4"
-              style={{textAlign: 'center'}}
+              style={{ textAlign: 'center' }}
             />
           </item>
-          </Grid>
-          <Grid item xs={4}><div className ="player">
-    <PoseTimer expiryTimestamp={time} />
-    {/* <OriginTimer expiryTimestamp={time} />  this was an experiment where I learned that the buttons don't
+        </Grid>
+        <Grid item xs={4}>
+          <div className="player">
+            <PoseTimer expiryTimestamp={time} />
+            {/* <OriginTimer expiryTimestamp={time} />  this was an experiment where I learned that the buttons don't
     work when it is position over the carousel, and only work when under*/}
-      </div></Grid>
+          </div>
+        </Grid>
         <Grid item xs={4}>
           <item>
             <IconButton
@@ -51,21 +56,19 @@ function SimpleDialog(props) {
               color="inherit"
               onClick={handleClose}
               aria-label="close"
-              style={{ color: "red" }}
+              style={{ color: 'red' }}
             >
-            <CloseIcon />
+              <CloseIcon />
               Close Routine
             </IconButton>
           </item>
         </Grid>
       </Grid>
-   <Grid>
-    <NewPlayerCarousel />
-    </Grid>
+      <Grid>
+        <NewPlayerCarousel routine={routine} />
+      </Grid>
     </Dialog>
   );
 }
- 
+
 export default SimpleDialog;
-
-
