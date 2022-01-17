602bb0908c8d3893033ed5c4ca1ffbfe93b2ae81
BeltedKingfisher <amyjseibert@gmail.com>
2022-01-16 18:20:43 -0600

    applied new timer logic

--- src/components/InfoDialog.js
+++ src/components/InfoDialog.js
@@ -1,6 +1,7 @@
 import { React, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -16,67 +17,44 @@ function InfoDialog(props) {
     updateRoutine,
     poseCounter,
     setPoseCounter,
-    addedTime2,
-    setAddedTime2,
-    routine
+    addedTime,
+    setAddedTime,
+    routine,
+    totalTime,
+    setTotalTime
   } = props;
 
-  const [currentIndex, setCurrentIndex] = useState(
-    routine.findIndex(pose => pose.id === selectedPose.id)
-  );
+  // const [currentValue, setCurrentValue] = useState(1);
 
   const handleClose = () => {
-    setModalOpen(false);
+    setModalOpen(!modalOpen);
+  };
+
+  const addTimeHandler = event => {
+    setAddedTime(Number(event.target.value) - selectedPose.defaultTime);
+    //setTotalTime(selectedPose.defaultTime + addedTime);
   };
 
   const handleAddToRoutine = e => {
     updateRoutine(prevState => {
       // Create new pose id so there are no repeating keys in poses
       let newPoseId = `pose-${poseCounter + 1}`;
-      let newPose = { ...selectedPose, id: newPoseId };
+      let newPose = {
+        ...selectedPose,
+        id: newPoseId,
+        addedTime: Number(addedTime)
+        //totalTime: Number(totalTime)
+      };
 
       prevState.push(newPose);
       return prevState;
     });
 
     setPoseCounter(poseCounter + 1);
-    setModalOpen(false);
-    // setAddedTime(0);
-  };
-
-  const addTimeHandler = event => {
-    // Check right column poses (routine state) to find the current pose's placement
-
-    setCurrentIndex(routine.findIndex(pose => pose.id === selectedPose.id));
-    //let currentIndex = routine.findIndex(pose => pose.id === selectedPose.id);
-
-    setAddedTime2(event.target.value);
-
-    console.log('current index', currentIndex);
-
-    // Update routine with newly timed pose
-    // setAddedTime(event.target.value);
-    let updatedPose = {
-      ...selectedPose,
-      addedTime: Number(event.target.value)
-    };
-
-    updateRoutine(prevRoutine => {
-      prevRoutine.splice(currentIndex, 1, updatedPose);
-      return prevRoutine;
-    });
-
-    console.log(event.target.value);
-    console.log('selected pose:', {
-      ...selectedPose,
-      addedTime: event.target.value
-    });
-
+    setModalOpen(!modalOpen);
+    setAddedTime(0);
+    //setTotalTime(0);
     console.log(routine);
-    console.log(
-      'routine added time at curr index: ',
-      routine[currentIndex].addedTime
-    );
   };
 
   return (
@@ -103,12 +81,19 @@ function InfoDialog(props) {
             className="textField"
             variant="outlined"
             margin="dense"
-            id="seconds"
-            label="Length of Pose"
+            id={selectedPose.id}
+            label="Seconds in pose"
             type="number"
-            defaultValue={selectedPose.defaultTime} //will eventually be {selectedPose.defaultTime}
-            value={routine[currentIndex].addedTime} //routine[currentIndex].addedTime
-            inputProps={{ min: 0, max: 10, step: 1 }}
+            placeholder={`${selectedPose.defaultTime} seconds`}
+            helperText={`Adjust time here.`}
+            // defaultValue={selectedPose.defaultTime}
+            //idea: helper text to give default time, then user chooses added seconds
+            //value={addedTime} //routine[currentIndex].addedTime
+            inputProps={{
+              min: selectedPose.defaultTime,
+              max: 500,
+              step: 1
+            }}
             onChange={addTimeHandler}
           />
           {/* needs capturing and sending over to Timer component */}
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,53 +1,18 @@
-// import { useEffect } from 'react';
+import { React, useState } from 'react';
 
-// const Timer = props => {
-//   const [timerIsRunning, setTimerisRunning] = useState(false);
-// const [timerSeconds, setTimerSeconds] = useState(33);
+const Timer = props => {
+  const { routine, selectedPose } = props;
 
-//   const {
-//     routine,
-//     timerIsRunning, //initialize to false
-//     timerSeconds, //setTimerSeconds(routine[index].defaultTime + routine[index].addedTime)
-//     setTimerSeconds,
-//     handleNextPhoto, //if/else or case/switch situation (carousel event?)
-//     resetTimer //resets setTimerSeconds to next index equation
-//   } = props;
+  return (
+    <div>
+      Time Displayed:
+      {routine.map(pose => {
+        let timeDisplay = pose.defaultTime + pose.addedTime;
+        //"mock" timer circle:
+        return <p key={pose.id}>{timeDisplay}</p>;
+      })}
+    </div>
+  );
+};
 
-//   // Referenced from https://upmostly.com/tutorials/build-a-react-timer-component-using-hooks
-//   useEffect(() => {
-//     let interval = null;
-
-//     if (timerIsRunning && timerSeconds !== 0) {
-//       interval = setInterval(() => {
-//         setTimerSeconds(prevSeconds => prevSeconds - 1);
-//       }, 1000);
-//     } else if (timerIsRunning && timerSeconds <= 0) {
-//       handleNextPhoto('Next');
-//       resetTimer();
-//     } else if (!timerIsRunning && timerSeconds !== 0) {
-//       clearInterval(interval);
-//     }
-
-//     // Clear interval if the component is unmounted
-//     return () => clearInterval(interval);
-//   }, [timerIsRunning, timerSeconds]);
-
-//   return <div className="Timer">:{timerSeconds}</div>;
-// };
-
-// export default Timer;
-
-// /*
-// http://react-responsive-carousel.js.org/storybook/?path=/story/02-advanced--with-external-controls
-// next = () => {
-//             this.setState((state) => ({
-//                 currentSlide: state.currentSlide + 1,
-//             }));
-//         };
-
-// prev = () => {
-//             this.setState((state) => ({
-//                 currentSlide: state.currentSlide - 1,
-//             }));
-//         };
-// */
+export default Timer;
